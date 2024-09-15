12ae8d69059b2af892f163f892232bf9e6a2f8ca
Precious Thandeka Bhembe <bhembepreciouspt@gmail.com>
2024-09-15 11:52:48 +0200

    Java work

--- src/chapter10/ex10_2_exercise/Order.java
+++ src/chapter10/ex10_2_exercise/Order.java
@@ -21,21 +21,17 @@ public class exercise2Order {
         return Double.toString(discount) + "%";
     }
 
-    // Code the calcDiscount method.
     public void calcDiscount() {
-        // Initialize discount to 0
         discount = 0.0;
 
-        // Check customer type and apply base discount
         if (custType == PRIVATE) {
-            discount = 5.0; // Private customers get a 5% discount
+            discount = 5.0;
         } else if (custType == NONPROFIT) {
-            discount = 10.0; // Non-profit customers get a 10% discount
+            discount = 10.0;
         }
 
-        // Additional discount if total is greater than 1000
         if (total > 1000) {
-            discount += 2.0; // Extra 2% discount for orders over $1000
+            discount += 2.0;
         }
     }
 }
